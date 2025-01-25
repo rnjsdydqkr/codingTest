398f4452e21cb6dfd5038c6a487e79f0de1f7cfb
Park Gwonyong <rnjsdydqkr@naver.com>
2025-01-25 17:41:42 +0900

    git connect

--- codingTest/main.swift
+++ codingTest/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+// ******************************
+// 그리디
+// ******************************
+var n = 1260
+var count = 0
 
+let array = [500, 100, 50, 10]
+
+for coin in array {
+    count += n / coin
+    n = n % coin
+}
+
+print(count)
