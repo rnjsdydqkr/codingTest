f637b12a103edfd5904dd914ee5bf8dfec6c3597
Park Kwonyong <rnjsdydqkr@naver.com>
2026-04-15 13:58:14 +0900

    배열 내 두 수의 합 찾기

--- codingTest/main.swift
+++ codingTest/main.swift
@@ -7,17 +7,55 @@
 
 import Foundation
 
+// *********************
+// 배열 내 두 수의 합 찾기
+// *********************
+
+//let input = readLine()!.split(separator: " ").map{ Int(String($0))! }
+//let target = Int(readLine()!)!
+//
+//func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+//    var dict = [Int: Int]() // [숫자: 인덱스] 형태의 딕셔너리
+//
+//    for (index, value) in nums.enumerated() {
+//        let needed = target - value
+//
+//        // 필요한 숫자가 이미 딕셔너리에 있다면 바로 반환
+//        if let foundIndex = dict[needed] {
+//            return [foundIndex, index]
+//        }
+//
+//        // 현재 숫자와 인덱스를 저장
+//        dict[value] = index
+//    }
+//
+//    return []
+//}
+
+//func twoSum(_ name: [Int], _ target: Int) -> [Int] {
+//  for i in 0..<name.count {
+//    for j in i + 1..<name.count {
+//      if name[i] + name[j] == target {
+//        return [i, j]
+//      }
+//    }
+//  }
+//  return []
+//}
+//
+//print(twoSum(input, target))
+
 // ******************************
 // 그리디
 // ******************************
-var n = 1260
-var count = 0
-
-let array = [500, 100, 50, 10]
-
-for coin in array {
-    count += n / coin
-    n = n % coin
-}
-
-print(count)
+//var n = 1260
+//var count = 0
+//
+//let array = [500, 100, 50, 10]
+//
+//for coin in array {
+//    count += n / coin
+//    n = n % coin
+//}
+//
+//print(count)
